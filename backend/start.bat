ec00e57b589fad4e6348fdea8f157c563d699d89
solomontjipto <solomontjipto@gmail.com>
2025-03-24 18:58:55 +1000

    improvemnt of life bat code

--- backend/start.bat
+++ backend/start.bat
@@ -2,7 +2,15 @@
 echo Starting server...
 SET PATH=%~dp0node;%PATH%
 cd %~dp0app
-node server.js
+@REM 
+
+REM Start server in a new terminal window so it doesn't block the rest
+start "" cmd /k "node server.js"
+
+REM Wait 2 seconds for server to boot
+timeout /t 2 >nul
+
 echo Starting server on Default Browser 
-start chrome https://localhost:3000
+start http://localhost:3000
+exit
 pause
